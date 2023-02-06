c51504ea5c3c7821f2af644c364d095272e18080
Anirban Mukherjee <43013843+amukherjee0506@users.noreply.github.com>
2023-02-05 20:26:04 -0800

    Added Camera Fix and Changed Background

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float FollowSpeed = 10f;
+    public Transform target;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -50f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+    }
+}
