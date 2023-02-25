63d3644d66f1dd2aa51724204ed71ec1701f8637
Vinay G Raj <vinaygraj@Navyas-MacBook-Pro.local>
2023-02-25 10:58:05 -0800

    Added eventOutcome attribute to Analytics + deleted unused code

--- Assets/Scripts/Analytics/AnalyticsData.cs
+++ Assets/Scripts/Analytics/AnalyticsData.cs
@@ -4,50 +4,10 @@ using UnityEngine;
 
 public class AnalyticsData
 {
-    // public int enemiesKilledTotalCount;
-    // public int level;
-    // public string reason;
-    // public string remEnergy;
-    // public long timestampLong;
-    // public int totalSuccessfulJumpsCount;
-    // public string type;
-    // public string sourceOfDeath;
-
     public int metricVersion; // later will help us to filter based on versions of data collection
     public int level;
     public long timestampLong;
     public string eventName;
     public string eventValue;
-
-
-    // public float velocity;
-
-    // public void setLevel(int level) {
-    //     this.level = level;
-    // }
-
-    // public void setEnemiesKilledTotalCount(int count) {
-    //     this.enemiesKilledTotalCount = count;
-    // }
-
-    // public void setReason(string reason) {
-    //     this.reason = reason;
-    // }
-
-    // public void setRemEnergy(string remEnergy) {
-    //     this.remEnergy = remEnergy;
-    // }
-
-    // public void setTimestampLong(long timestampLong) {
-    //     this.timestampLong = timestampLong;
-    // }
-
-    // public void setTotalSuccessfulJumpsCount(int count) {
-    //     this.totalSuccessfulJumpsCount = count;
-    // }
-
-    // public void setType(string type) {
-    //     this.type = type;
-    // }
-    
+    public string eventOutcome;   
 }
--- Assets/Scripts/Analytics/StatisticsManager.cs
+++ Assets/Scripts/Analytics/StatisticsManager.cs
@@ -15,7 +15,7 @@ public class StatisticsManager
         RestClient.Post(database_url, analyticsData);
     }
 
-    public static void buildAnaltyicsDataObjAndPush(string eventName, string eventValue){
+    public static void buildAnaltyicsDataObjAndPush(string eventName, string eventValue, string eventOutcome){
         AnalyticsData analytics_data = new AnalyticsData();
         
         analytics_data.metricVersion = Config.metricVersion;
@@ -23,41 +23,10 @@ public class StatisticsManager
         analytics_data.timestampLong = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
         analytics_data.eventName = eventName;
         analytics_data.eventValue = eventValue;
+        analytics_data.eventOutcome = eventOutcome;
 
         StatisticsManager statisticsManager = new StatisticsManager();
         statisticsManager.pushDataToAnalyticsDb(analytics_data);
     }
 
-    // // if type is dummy then it is a dummy object
-    // public static void buildAnaltyicsDataObjAndPush(int enemiesKilledTotalCount = 0, int level = 0, string reason = "dummy", string remEnergy = "dummy", int totalSuccessfulJumpsCount = 0, string type = "dummy", string sourceOfDeath = "dummy"){
-    //     AnalyticsData analytics_data = new AnalyticsData();
-        
-    //     analytics_data.enemiesKilledTotalCount = enemiesKilledTotalCount;
-    //     analytics_data.level = level;
-    //     analytics_data.reason = reason;
-    //     analytics_data.remEnergy = remEnergy;
-    //     analytics_data.timestampLong = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
-    //     analytics_data.type = type;
-    //     analytics_data.totalSuccessfulJumpsCount = totalSuccessfulJumpsCount;
-    //     analytics_data.sourceOfDeath = sourceOfDeath;
-
-    //     StatisticsManager statisticsManager = new StatisticsManager();
-    //     statisticsManager.pushDataToAnalyticsDb(analytics_data);
-    // }
-
-    // public static void buildAnaltyicsDataObjAndPush() {
-    //     AnalyticsData analytics_data = new AnalyticsData();
-        
-    //     analytics_data.enemiesKilledTotalCount = 0;
-    //     analytics_data.level = 1;
-    //     analytics_data.reason = "DieOnSpace";
-    //     analytics_data.remEnergy = "0%";
-    //     analytics_data.timestampLong = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
-    //     analytics_data.type = "termination";
-    //     analytics_data.totalSuccessfulJumpsCount = 0;
-    //     analytics_data.sourceOfDeath = "Enemy";
-
-    //     StatisticsManager statisticsManager = new StatisticsManager();
-    //     statisticsManager.pushDataToAnalyticsDb(analytics_data);
-    // }
 }
