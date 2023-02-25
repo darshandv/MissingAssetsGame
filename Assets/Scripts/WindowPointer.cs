5d27e099b9edd1b621f7fa476069a2e3491f88ee
makhtar18 <122763616+makhtar18@users.noreply.github.com>
2023-02-25 15:04:41 -0800

    Quest Pointer

---
+++ Assets/Scripts/WindowPointer.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using UnityEngine.UI;
+
+public class WindowPointer : MonoBehaviour
+{
+    [SerializeField] private Camera uiCamera;
+    [SerializeField] private Sprite arrow;
+    [SerializeField] private Sprite cross;
+    private Vector3 targetPosition;
+    public Transform pointerRectTransform;
+    public Transform goalPlanet;
+    public Transform player;
+    private Image pointerImage;
+    private void Awake()
+    {
+        targetPosition = goalPlanet.position;
+        pointerImage = transform.Find("Pointer").GetComponent<Image>();
+    }
+    private void Update()
+    {
+        Debug.Log("target Pos " + targetPosition);
+        Vector3 toPosition = targetPosition;
+        Vector3 fromPosition = player.position;
+        fromPosition.z = 0f;
+        Vector3 dir = (toPosition - fromPosition).normalized;
+        float angle = UtilsClass.GetAngleFromVectorFloat(dir);
+        pointerRectTransform.localEulerAngles = new Vector3(0, 0, angle);
+
+
+        float borderSize = 70f;
+        Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(targetPosition);
+        bool isOffScreen = targetPositionScreenPoint.x <= borderSize || targetPositionScreenPoint.x >= Screen.width - borderSize || targetPositionScreenPoint.y <= borderSize || targetPositionScreenPoint.y >= Screen.height - borderSize;
+        Debug.Log("IsOffScreen "+isOffScreen);
+        if (isOffScreen)
+        {
+            pointerImage.sprite = arrow;
+            Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
+            if (cappedTargetScreenPosition.x <= borderSize) cappedTargetScreenPosition.x = borderSize;
+            if (cappedTargetScreenPosition.x >= Screen.width - borderSize) cappedTargetScreenPosition.x = Screen.width - borderSize ;
+            if (cappedTargetScreenPosition.y <= borderSize) cappedTargetScreenPosition.y = borderSize;
+            if (cappedTargetScreenPosition.y >= Screen.height - borderSize) cappedTargetScreenPosition.y = Screen.height - borderSize;
+            Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(cappedTargetScreenPosition);
+            pointerRectTransform.position = pointerWorldPosition;
+            pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f);
+        } else
+        {
+            pointerImage.sprite = cross;
+            Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(targetPositionScreenPoint);
+            pointerRectTransform.position = pointerWorldPosition;
+            pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f);
+        }
+    }
+}
