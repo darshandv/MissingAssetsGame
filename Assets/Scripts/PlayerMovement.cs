e946f2f6911a23e7a8730ba7d8e0daff68697109
makhtar18 <122763616+makhtar18@users.noreply.github.com>
2023-04-03 13:23:39 -0700

    Adding Boundary till level 4

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -96,6 +96,10 @@ public class PlayerMovement : MonoBehaviour
                 reduceHealth(missileDamage);
                 Debug.Log("MISSILE DAMAGE TRIGGERED");
             }
+            else if (collision.collider.CompareTag("Boundary"))
+            { 
+                Debug.Log("Reached Boundary");
+            }
             else
             {
                 //enemy bullets
