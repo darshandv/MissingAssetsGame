56eade0f365ed4f5a2f8859e9023ee3a7a94faf0
makhtar18 <122763616+makhtar18@users.noreply.github.com>
2023-04-03 17:21:32 -0700

    Fixed Collectible bar count

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -33,7 +33,7 @@ public static class Config
     };
     public static int currentLevel = 1;
 
-    public static int[] levelCollectibles = { 1, 3, 4, 7, 6, 6, 7, 3, 2, 3, 3 };
+    public static int[] levelCollectibles = { 1, 3, 4, 7, 6, 6, 7, 4, 2, 3, 3 };
 
     public static void ResetAllVariables()
     {
