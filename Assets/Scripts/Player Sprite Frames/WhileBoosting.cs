573d18bb248e4505d264d199ea5e52c6db7fe8e5
Anirban Mukherjee <43013843+amukherjee0506@users.noreply.github.com>
2023-04-10 00:05:00 -0700

    Scene 1 Updated
    Added boost depleted features

--- Assets/Scripts/Player Sprite Frames/WhileBoosting.cs
+++ Assets/Scripts/Player Sprite Frames/WhileBoosting.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WhileBoosting : MonoBehaviour
 {
+    public PlayerMovement Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +14,14 @@ public class WhileBoosting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!(Input.GetKey(KeyCode.W)))
-        {
-            gameObject.GetComponent<Renderer>().enabled = false;
-        }
-        else
+        
+        if ((Input.GetKey(KeyCode.W)) && (((Player.tc.getThrust())>0.5f)))
         {
             gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        else 
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
         }
     }
 }
